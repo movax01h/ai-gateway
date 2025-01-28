khulnasoft-lab/ai-assist

feat: make duo chat aware of current date

ai_gateway/chat/agents/react.py
@@ -43,6 +43,7 @@ class ReActAgentInputs(BaseModel):
     model_metadata: Optional[ModelMetadata] = None
     unavailable_resources: Optional[list[str]] = None
     tools: Optional[list[BaseTool]] = None
+    current_date: Optional[str] = None
 
 
 class ReActPlainTextParser(BaseCumulativeTransformOutputParser):
@@ -153,6 +154,7 @@ class ReActAgent(Prompt[ReActAgentInputs, TypeAgentEvent]):
                         prompt_template["system"],
                         tools=agent_inputs.tools,
                         unavailable_resources=agent_inputs.unavailable_resources,
+                        current_date=agent_inputs.current_date,
                     )
                 )
             )

movax01h/ai-gateway

ai_gateway/chat/agents/react.py
@@ -45,6 +45,7 @@ class ReActAgentInputs(BaseModel):
     unavailable_resources: Optional[list[str]] = None
     tools: Optional[list[BaseTool]] = None
     conciseness_prompt_change_active: Optional[bool] = None
+    current_date: Optional[str] = None
 
 
 class ReActPlainTextParser(BaseCumulativeTransformOutputParser):
@@ -148,6 +149,7 @@ class ReActPromptTemplate(Runnable[ReActAgentInputs, PromptValue]):
                 tools=input.tools,
                 unavailable_resources=input.unavailable_resources,
                 conciseness_prompt_change_active=input.conciseness_prompt_change_active,
+                current_date=input.current_date,
             )
             if is_feature_enabled(FeatureFlag.ENABLE_ANTHROPIC_PROMPT_CACHING):
                 content = [