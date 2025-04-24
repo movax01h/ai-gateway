d985c971df1d29f64be4c68dad7aa9940db5eb11
Shinya Maeda <shinya@gitlab.com>
2025-04-24 16:26:31 +0900

    fix: make dws opt-in in ai gateway image

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,15 +6,16 @@ if [ -n "$WEB_CONCURRENCY" ] && [ "$WEB_CONCURRENCY" -gt 1 ]; then
   export PROMETHEUS_MULTIPROC_DIR=$METRICS_DIR
 fi
 
-# Run both AI Gateway and Duo Workflow Service by default.
-# Optionally, you can disable a service by setting `DISABLE_AI_GATEWAY` or `DISABLE_DUO_WORKFLOW_SERVICE` variable.
+# Run AI Gateway by default.
+# Optionally, you can run Duo Workflow Service by setting `ENABLE_DUO_WORKFLOW_SERVICE=true` variable.
+# Optionally, you can disable AI Gateway by setting `DISABLE_AI_GATEWAY=true` variable.
 commands=()
 
 if [[ -z "${DISABLE_AI_GATEWAY}" ]]; then
   commands+=("poetry run ai_gateway")
 fi
 
-if [[ -z "${DISABLE_DUO_WORKFLOW_SERVICE}" ]]; then
+if [[ -n "${ENABLE_DUO_WORKFLOW_SERVICE}" ]]; then
   commands+=("poetry run duo-workflow-service")
 fi
 
