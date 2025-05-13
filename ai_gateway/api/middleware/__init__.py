aface330cf61a70c9de37ecd4725c747a28afc42
G S <gnaut3@gmail.com>
2025-05-13 14:20:11 +0000

    refactor: move MiddlewareAuthentication to authentication.py

--- ai_gateway/api/middleware/__init__.py
+++ ai_gateway/api/middleware/__init__.py
@@ -1,8 +1,5 @@
-from .base import (
-    AccessLogMiddleware,
-    DistributedTraceMiddleware,
-    MiddlewareAuthentication,
-)
+from .authentication import MiddlewareAuthentication
+from .base import AccessLogMiddleware, DistributedTraceMiddleware
 from .feature_flag import FeatureFlagMiddleware
 from .headers import (
     X_GITLAB_CLIENT_NAME,
@@ -26,24 +23,24 @@ from .model_config import ModelConfigMiddleware
 
 __all__ = [
     "AccessLogMiddleware",
-    "FeatureFlagMiddleware",
-    "MiddlewareAuthentication",
-    "InternalEventMiddleware",
     "DistributedTraceMiddleware",
+    "FeatureFlagMiddleware",
+    "InternalEventMiddleware",
+    "MiddlewareAuthentication",
     "ModelConfigMiddleware",
-    "X_GITLAB_REALM_HEADER",
-    "X_GITLAB_INSTANCE_ID_HEADER",
-    "X_GITLAB_GLOBAL_USER_ID_HEADER",
-    "X_GITLAB_TEAM_MEMBER_HEADER",
-    "X_GITLAB_HOST_NAME_HEADER",
-    "X_GITLAB_VERSION_HEADER",
-    "X_GITLAB_SAAS_DUO_PRO_NAMESPACE_IDS_HEADER",
-    "X_GITLAB_FEATURE_ENABLED_BY_NAMESPACE_IDS_HEADER",
-    "X_GITLAB_FEATURE_ENABLEMENT_TYPE_HEADER",
-    "X_GITLAB_MODEL_GATEWAY_REQUEST_SENT_AT",
-    "X_GITLAB_LANGUAGE_SERVER_VERSION",
+    "X_GITLAB_CLIENT_NAME",
     "X_GITLAB_CLIENT_TYPE",
     "X_GITLAB_CLIENT_VERSION",
-    "X_GITLAB_CLIENT_NAME",
+    "X_GITLAB_FEATURE_ENABLED_BY_NAMESPACE_IDS_HEADER",
+    "X_GITLAB_FEATURE_ENABLEMENT_TYPE_HEADER",
+    "X_GITLAB_GLOBAL_USER_ID_HEADER",
+    "X_GITLAB_HOST_NAME_HEADER",
+    "X_GITLAB_INSTANCE_ID_HEADER",
     "X_GITLAB_INTERFACE",
+    "X_GITLAB_LANGUAGE_SERVER_VERSION",
+    "X_GITLAB_MODEL_GATEWAY_REQUEST_SENT_AT",
+    "X_GITLAB_REALM_HEADER",
+    "X_GITLAB_SAAS_DUO_PRO_NAMESPACE_IDS_HEADER",
+    "X_GITLAB_TEAM_MEMBER_HEADER",
+    "X_GITLAB_VERSION_HEADER",
 ]
