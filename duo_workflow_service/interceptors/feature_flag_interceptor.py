4ebf790fb68e11057e0c4ae214020dd86430a767
AndrasHerczeg <aherczeg@gitlab.com>
2025-06-05 16:37:48 +0200

    chore: Revert changes related to the websocket server

--- duo_workflow_service/interceptors/feature_flag_interceptor.py
+++ duo_workflow_service/interceptors/feature_flag_interceptor.py
@@ -1,39 +1,17 @@
-from typing import Dict, Optional, Set
+from typing import Optional
 
 import grpc
-from fastapi import WebSocket
 
 from duo_workflow_service.interceptors import X_GITLAB_REALM_HEADER
-from duo_workflow_service.interceptors.websocket_middleware import WebSocketMiddleware
 from lib.feature_flags.context import current_feature_flag_context
-
-X_GITLAB_ENABLED_FEATURE_FLAGS = "x-gitlab-enabled-feature-flags"
-
-
-def _set_feature_flag_context(
-    metadata: Dict[str, str], disallowed_flags: Optional[Dict[str, Set[str]]] = None
-):
-    # Extract enabled feature flags from metadata
-    enabled_feature_flags = set(
-        metadata.get(X_GITLAB_ENABLED_FEATURE_FLAGS, "").split(",")
-    )
-
-    if disallowed_flags:
-        # Remove feature flags that are not supported in the specific realm.
-        gitlab_realm = metadata.get(X_GITLAB_REALM_HEADER, "")
-        disallowed_flags_for_realm = disallowed_flags.get(gitlab_realm, set())
-        enabled_feature_flags = enabled_feature_flags.difference(
-            disallowed_flags_for_realm
-        )
-
-    # Set feature flags in context
-    current_feature_flag_context.set(enabled_feature_flags)
 
 
 class FeatureFlagInterceptor(grpc.aio.ServerInterceptor):
     """Interceptor that handles feature flags propagation."""
 
-    def __init__(self, disallowed_flags: Optional[Dict[str, Set[str]]] = None):
+    X_GITLAB_ENABLED_FEATURE_FLAGS = "x-gitlab-enabled-feature-flags"
+
+    def __init__(self, disallowed_flags: Optional[dict] = None):
         self.disallowed_flags = disallowed_flags
 
     async def intercept_service(
@@ -44,20 +22,19 @@ class FeatureFlagInterceptor(grpc.aio.ServerInterceptor):
         """Intercept incoming requests to inject feature flags context."""
         metadata = dict(handler_call_details.invocation_metadata)
 
-        _set_feature_flag_context(metadata, self.disallowed_flags)
+        # Extract enabled feature flags from metadata
+        enabled_feature_flags = metadata.get(
+            self.X_GITLAB_ENABLED_FEATURE_FLAGS, ""
+        ).split(",")
+        enabled_feature_flags = set(enabled_feature_flags)
+
+        if self.disallowed_flags:
+            # Remove feature flags that are not supported in the specific realm.
+            gitlab_realm = metadata.get(X_GITLAB_REALM_HEADER, "")
+            disallowed_flags = self.disallowed_flags.get(gitlab_realm, set())
+            enabled_feature_flags = enabled_feature_flags.difference(disallowed_flags)
+
+            # Set feature flags in context
+        current_feature_flag_context.set(enabled_feature_flags)
 
         return await continuation(handler_call_details)
-
-
-class FeatureFlagMiddleware(WebSocketMiddleware):
-    """Middleware that handles feature flags propagation for WebSockets."""
-
-    def __init__(self, disallowed_flags: Optional[Dict[str, Set[str]]] = None):
-        self.disallowed_flags = disallowed_flags
-
-    async def __call__(self, websocket: WebSocket):
-        """Process WebSocket connection to inject feature flags context."""
-        # Extract headers from WebSocket
-        headers = dict(websocket.headers)
-
-        _set_feature_flag_context(headers, self.disallowed_flags)
--- duo_workflow_service/servers/__init__.py
+++
@@ -1,7 +0,0 @@
-from duo_workflow_service.servers.grpc_server import grpc_serve
-from duo_workflow_service.servers.websocket_server import websocket_serve
-
-__all__ = [
-    "grpc_serve",
-    "websocket_serve",
-]
