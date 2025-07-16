d7c4c817f5c460f6a6f06a532d9db5eb644747c1
Mohamed Hamda <mhamda@gitlab.com>
2025-07-16 09:50:37 +0200

    fix: Fix explain issues with large files

--- ai_gateway/api/middleware/model_config.py
+++ ai_gateway/api/middleware/model_config.py
@@ -23,24 +23,42 @@ class ModelConfigMiddleware:
             return
 
         async def fetch_model_metadata() -> Message:
-            message = await receive()
+            body_parts = []
+            max_chunks = 1000
+            chunk_count = 0
 
-            body: bytes = message.get("body", b"")
+            while chunk_count < max_chunks:
+                chunk_count += 1
+                message = await receive()
 
-            if b"model_metadata" not in body:
-                return message
+                if message["type"] == "http.request":
+                    body_part = message.get("body", b"")
+                    if body_part:
+                        body_parts.append(body_part)
 
-            body_str: str = body.decode("utf-8")
+                    if not message.get("more_body", False):
+                        break
+                elif body_parts:
+                    continue
+                else:
+                    return message
+
+            full_body = b"".join(body_parts) if body_parts else b""
+
+            if b"model_metadata" not in full_body:
+                return {"type": "http.request", "body": full_body, "more_body": False}
 
             try:
+                body_str = full_body.decode("utf-8")
                 data = json.loads(body_str)
-            except json.JSONDecodeError:
-                data = {}
 
-            if "model_metadata" in data:
-                current_model_metadata_context.set(
-                    create_model_metadata(data["model_metadata"])
-                )
-            return message
+                if "model_metadata" in data:
+                    model_metadata = create_model_metadata(data["model_metadata"])
+                    current_model_metadata_context.set(model_metadata)
+
+            except (json.JSONDecodeError, UnicodeDecodeError):
+                pass
+
+            return {"type": "http.request", "body": full_body, "more_body": False}
 
         await self.app(scope, fetch_model_metadata, send)
