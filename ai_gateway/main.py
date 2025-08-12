f97ebec1e4c72c4e532cb33182dee9e9f75610e8
Wanderson Policarpo <wpolicarpo@gitlab.com>
2025-08-12 18:13:19 +0100

    fix: Fix logging and metrics setup when hot reloading is enabled

--- ai_gateway/main.py
+++ ai_gateway/main.py
@@ -1,46 +1,10 @@
-import logging
-import os
-
 import uvicorn
-from prometheus_client import (
-    REGISTRY,
-    CollectorRegistry,
-    multiprocess,
-    start_http_server,
-)
 
-from ai_gateway.app import Config, get_config
-from ai_gateway.structured_logging import setup_logging
-
-
-def start_metrics_server(config: Config):
-    log = logging.getLogger("main")
-    log.info(
-        "Metrics HTTP server running on http://%s:%d",
-        config.fastapi.metrics_host,
-        config.fastapi.metrics_port,
-    )
-
-    registry = REGISTRY
-
-    # pylint: disable=direct-environment-variable-reference
-    if "PROMETHEUS_MULTIPROC_DIR" in os.environ:
-        registry = CollectorRegistry()
-        multiprocess.MultiProcessCollector(registry)
-    # pylint: enable=direct-environment-variable-reference
-
-    start_http_server(
-        addr=config.fastapi.metrics_host,
-        port=config.fastapi.metrics_port,
-        registry=registry,
-    )
+from ai_gateway.app import get_config
 
 
 def run_app():
     config = get_config()
-
-    setup_logging(config.logging, config.custom_models.enabled)
-    start_metrics_server(config)
 
     # For now, trust all IPs for proxy headers until https://github.com/encode/uvicorn/pull/1611 is available.
     uvicorn.run(
