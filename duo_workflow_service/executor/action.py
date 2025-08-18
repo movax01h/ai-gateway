2b08270051e901cbf4f5258588d8bdbd928288f0
Shinya Maeda <shinya@gitlab.com>
2025-08-18 16:25:44 +0900

    fix: revert timeout on execute_action_and_get_action_response

--- duo_workflow_service/executor/action.py
+++ duo_workflow_service/executor/action.py
@@ -6,16 +6,12 @@ import structlog
 from prometheus_client import Histogram
 
 from contract import contract_pb2
-from duo_workflow_service.tracking import log_exception
 
 ACTION_LATENCY = Histogram(
     name="executor_actions_duration_seconds",
     documentation="Latency for all actions that go to the Executor.",
     labelnames=["action_class"],
 )
-
-TIMEOUT_OUTBOX_PUT = 60
-TIMEOUT_INBOX_GET = 60
 
 
 def record_metrics(action_class: str, duration: float):
@@ -38,33 +34,8 @@ async def _execute_action_and_get_action_response(
     )
 
     start_time = time.time()
-    try:
-        log.debug("Waiting for putting action to outbox...", extra={"source": __name__})
-        # `outbox.put` could hung indefinitely if the queue is full and no tasks get an item from it.
-        # There are several cases when this could happen:
-        # - Connection between client and server is terminated during the workflow is running
-        # - Deadlock between workflow and client-server message loop
-        # TODO: Handle the `asyncio.TimeoutError` properly.
-        await asyncio.wait_for(outbox.put(action), TIMEOUT_OUTBOX_PUT)
-    except asyncio.TimeoutError as ex:
-        log_exception(ex)
-        raise
-
-    try:
-        log.debug(
-            "Waiting for getting action from inbox...", extra={"source": __name__}
-        )
-        # `inbox.get` could hung indefinitely if the queue is full and no tasks get an item from it.
-        # There are several cases when this could happen:
-        # - Connection between client and server is terminated during the workflow is running
-        # - Deadlock between workflow and client-server message loop
-        # TODO: Handle the `asyncio.TimeoutError` properly.
-        event: contract_pb2.ClientEvent = await asyncio.wait_for(
-            inbox.get(), TIMEOUT_INBOX_GET
-        )
-    except asyncio.TimeoutError as ex:
-        log_exception(ex)
-        raise
+    await outbox.put(action)
+    event: contract_pb2.ClientEvent = await inbox.get()
 
     if event.actionResponse:
         duration = time.time() - start_time
