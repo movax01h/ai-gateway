4dbba4f8432b879dab3f3876df82c86e9b03fd77
Junming Huang <juhuang@gitlab.com>
2025-08-22 16:51:06 +0200

    feat: update run command input schema

--- tests/duo_workflow_service/tools/test_run_command.py
+++ tests/duo_workflow_service/tools/test_run_command.py
@@ -1,3 +1,4 @@
+from typing import List
 from unittest import mock
 from unittest.mock import AsyncMock, MagicMock
 
@@ -8,7 +9,24 @@ from duo_workflow_service.tools.command import RunCommand, RunCommandInput
 
 
 @pytest.mark.asyncio
-async def test_run_command_success():
+@pytest.mark.parametrize(
+    ("program", "args", "expected_action_args"),
+    [
+        (
+            "poetry",
+            " run uvicorn  main:app --host 0.0.0.0 --port 8018 ",
+            ["run", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8018"],
+        ),
+        (
+            "pytest",
+            "  tests/test_main.py::test_app_start ",
+            ["tests/test_main.py::test_app_start"],
+        ),
+    ],
+)
+async def test_run_command_success(
+    program: str, args: str, expected_action_args: List[str]
+):
     mock_outbox = MagicMock()
     mock_outbox.put = AsyncMock()
 
@@ -24,53 +42,15 @@ async def test_run_command_success():
     run_command = RunCommand(name="run_command", description="Run a shell command")
     run_command.metadata = metadata
 
-    program = "echo"
-    args = ["Hello"]
-    flags: list[str] = []
-    expected_response = "done"
+    response = await run_command._arun(program=program, args=args)
 
-    response = await run_command._arun(program=program, arguments=args, flags=flags)
-
-    assert response == expected_response
+    assert response == "done"
 
     mock_outbox.put.assert_called_once()
     action = mock_outbox.put.call_args[0][0]
     assert action.runCommand.program == program
-    assert action.runCommand.arguments == args
-    assert action.runCommand.flags == flags
-
-
-@pytest.mark.asyncio
-async def test_run_command_with_flags_success():
-    mock_outbox = MagicMock()
-    mock_outbox.put = AsyncMock()
-
-    mock_inbox = MagicMock()
-    mock_inbox.get = AsyncMock(
-        return_value=contract_pb2.ClientEvent(
-            actionResponse=contract_pb2.ActionResponse(response="done")
-        )
-    )
-
-    metadata = {"outbox": mock_outbox, "inbox": mock_inbox}
-
-    run_command = RunCommand(name="run_command", description="Run a shell command")
-    run_command.metadata = metadata
-
-    program = "ls"
-    args = ["/home"]
-    flags = ["-l", "-a"]
-    expected_response = "done"
-
-    response = await run_command._arun(program=program, arguments=args, flags=flags)
-
-    assert response == expected_response
-
-    mock_outbox.put.assert_called_once()
-    action = mock_outbox.put.call_args[0][0]
-    assert action.runCommand.program == program
-    assert action.runCommand.arguments == args
-    assert action.runCommand.flags == flags
+    assert action.runCommand.arguments == expected_action_args
+    assert action.runCommand.flags == []
 
 
 @pytest.mark.asyncio
@@ -84,26 +64,18 @@ async def test_run_command_with_flags_success():
     ],
 )
 @mock.patch("duo_workflow_service.tools.command._execute_action")
-async def test_run_git_command(execute_action_mock, program):
+async def test_run_disallowed_command(execute_action_mock, program):
     run_command = RunCommand(name="run_command", description="Run a shell command")
 
-    await run_command._arun(program=program)
+    await run_command._arun(program=program, args="")
 
     execute_action_mock.assert_not_called()
-
-
-@pytest.mark.asyncio
-async def test_run_command_not_implemented_error():
-    run_command = RunCommand(name="run_command", description="Run a shell command")
-
-    with pytest.raises(NotImplementedError):
-        run_command._run("", [], [])
 
 
 def test_run_command_format_display_message():
     tool = RunCommand(description="Run a shell command")
 
-    input_data = RunCommandInput(program="ls", arguments=["/home"], flags=["-l", "-a"])
+    input_data = RunCommandInput(program="ls", args="-l -a /home ")
 
     message = tool.format_display_message(input_data)
 
