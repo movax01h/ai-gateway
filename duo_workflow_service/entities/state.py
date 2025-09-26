dcc71d8b7c15d09930f350c58d3c71451439c819
Halil Coban <hcoban@gitlab.com>
2025-09-26 11:13:04 +0200

    chore: set max tokens to 400K

--- duo_workflow_service/entities/state.py
+++ duo_workflow_service/entities/state.py
@@ -19,8 +19,8 @@ from duo_workflow_service.token_counter.approximate_token_counter import (
 )
 from duo_workflow_service.workflows.type_definitions import AdditionalContext
 
-# max content tokens is 1M but adding a buffer of 10% just in case
-MAX_CONTEXT_TOKENS = int(1_000_000 * 0.90)
+# max content tokens is 400K but adding a buffer of 10% just in case
+MAX_CONTEXT_TOKENS = int(400_000 * 0.90)
 MAX_SINGLE_MESSAGE_TOKENS = int(MAX_CONTEXT_TOKENS * 0.65)
 
 logger = structlog.stdlib.get_logger("workflow")
