fe088011fb9a35d735380fbaa7495401fac71275
KG Gadgil <kgadgil@gitlab.com>
2026-02-10 08:25:50 -0500

    fix(duo-workflow): clear additional_context from logged start request

--- duo_workflow_service/server.py
+++ duo_workflow_service/server.py
@@ -151,10 +151,11 @@ def has_sufficient_usage_quota(event: UsageQuotaEvent):
 def clean_start_request(start_workflow_request: contract_pb2.ClientEvent):
     request = contract_pb2.ClientEvent()
     request.CopyFrom(start_workflow_request)
-    # Remove the goal from being logged to prevent logging sensitive user content
+    # Remove fields from being logged to prevent logging sensitive user content
     request.startRequest.ClearField("goal")
     request.startRequest.ClearField("flowConfig")
     request.startRequest.ClearField("workflowMetadata")
+    request.startRequest.ClearField("additional_context")
     return request
 
 
