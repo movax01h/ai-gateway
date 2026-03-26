9286187bb2ccc2a6cc61290f799c31deec46a896
Omar Qunsul <oqunsul@gitlab.com>
2026-03-26 15:10:03 +0100

    chore: fix monitoring/ready after switching default code completion model

--- ai_gateway/prompts/base.py
+++ ai_gateway/prompts/base.py
@@ -658,7 +658,11 @@ class BasePromptRegistry(ABC):
             # TODO: Remove this exception once the prompt registry properly supports Fireworks.
             # See https://gitlab.com/gitlab-org/modelops/applied-ml/code-suggestions/ai-assist/-/work_items/906
             self.validations = set(
-                {"codestral_2501_fireworks", "text_embedding_005_vertex"}
+                {
+                    "codestral_2501_fireworks",
+                    "codestral_2508_fireworks",
+                    "text_embedding_005_vertex",
+                }
             )
 
         # Collect invocations to execute them in parallel
