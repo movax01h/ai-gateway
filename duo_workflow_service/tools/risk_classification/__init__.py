a84bb1f192e797851b606944f0ced10b75310d1a
Wanderson Policarpo <wpolicarpo@gitlab.com>
2026-08-31 12:14:49 +0000

    feat(tools): add submit_merge_request_risk_classification tool

---
+++ duo_workflow_service/tools/risk_classification/__init__.py
@@ -0,0 +1,11 @@
+from .submit_merge_request_risk_classification import (
+    RiskClassificationClaimInput,
+    SubmitMergeRequestRiskClassification,
+    SubmitMergeRequestRiskClassificationInput,
+)
+
+__all__ = [
+    "RiskClassificationClaimInput",
+    "SubmitMergeRequestRiskClassification",
+    "SubmitMergeRequestRiskClassificationInput",
+]
